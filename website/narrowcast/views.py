396cafb2854d8c43042219a092e6a604b45acfdc
chaos128 <chaos0128@gmail.com>
2013-01-22 11:13:47 +0900

    Initial commit of website by Flask

--- website/narrowcast/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render_to_response
-from django.template import RequestContext, TemplateDoesNotExist
-
-def home(request):
-    """Shows the home page."""
-    return render_to_response('base.html', context_instance=RequestContext(request))
